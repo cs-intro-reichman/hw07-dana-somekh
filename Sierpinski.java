5e05dfd8a3e02597e8f5ddcc17a88b596fff7fa1
Dana Somekh <dana.somekh@post.runi.ac.il>
2025-12-30 22:03:48 +0200

    Completed assignment

--- Sierpinski.java
+++ Sierpinski.java
@@ -7,12 +7,45 @@ public class Sierpinski {
 	
 	// Draws a Sierpinski triangle of depth n on the standard canvass.
 	public static void sierpinski (int n) {
-		//// Replace this comment with your code
+		StdDraw.setScale(0.0, 1.0);
+		StdDraw.clear(StdDraw.WHITE);
+
+		double x1 = 0.5,  y1 = 0.95;  
+		double x2 = 0.05, y2 = 0.05;  
+		double x3 = 0.95, y3 = 0.05;  
+
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.line(x1, y1, x2, y2);
+		StdDraw.line(x2, y2, x3, y3);
+		StdDraw.line(x3, y3, x1, y1);
+
+		sierpinski(n, x1, x2, x3, y1, y2, y3);
 	}
 	
 	// Does the actual drawing, recursively.
 	private static void sierpinski(int n, double x1, double x2, double x3,
 		                                 double y1, double y2, double y3) {
-		//// Replace this comment with your code
-	}
+		if (n == 0) return;
+
+		double mx12 = (x1 + x2) / 2.0;
+		double my12 = (y1 + y2) / 2.0;
+
+		double mx23 = (x2 + x3) / 2.0;
+		double my23 = (y2 + y3) / 2.0;
+
+		double mx31 = (x3 + x1) / 2.0;
+		double my31 = (y3 + y1) / 2.0;
+
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.filledPolygon(
+			new double[]{mx12, mx23, mx31},
+			new double[]{my12, my23, my31}
+		);
+
+		StdDraw.setPenColor(StdDraw.BLACK);
+
+		sierpinski(n - 1, x1, mx12, mx31, y1, my12, my31); 
+		sierpinski(n - 1, mx12, x2, mx23, my12, y2, my23); 
+		sierpinski(n - 1, mx31, mx23, x3, my31, my23, y3); 
+		}
 }
